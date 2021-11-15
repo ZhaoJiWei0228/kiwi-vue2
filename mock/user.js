898f7c8398db66ef27cc924f58fd5b4d407088b2
zhaojiwei_v <zhaojiwei_v@didiglobal.com>
2021-11-15 10:18:18 +0800

    合规完善

--- mock/user.js
+++ mock/user.js
@@ -8,26 +8,7 @@ module.exports = {
         'email': '892254112@qq.com',
         'menuAuth': [
             {
-                'auth': 0,
-                'icon': 'scene',
-                'id': 1012971,
-                'isMenu': 1,
-                'menus': [
-                    {
-                        'auth': 0,
-                        'icon': '',
-                        'id': 1012972,
-                        'isMenu': 1,
-                        'name': '场景列表',
-                        'pid': 1012971,
-                        'sortVal': 10,
-                        'url': '/manage/scene/list'
-                    }
-                ],
-                'name': '场景管理',
-                'pid': 0,
-                'sortVal': 20,
-                'url': '/manage/scene/business'
+                
             }
         ],
         'realname': '水鸟',
